2957e8e7c6c10491daad2de0accf9c93e7919a11
Maheswar Siva <maheswarsiva98@gmail.com>
2022-05-06 11:37:46 +0530

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:freeze_app/SignIn/register.dart';
+import 'package:freeze_app/SignIn/signin.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Register(),
+      home: const Signin(),
     );
   }
 }
